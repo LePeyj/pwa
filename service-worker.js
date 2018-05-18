d44b490b994f9400526f92bc7fa4b4f84f742e37
philippglenn <peyj73@gmail.com>
2018-05-18 11:57:31 +0800

    added

--- service-worker.js
+++ service-worker.js
@@ -20,17 +20,28 @@ self.addEventListener('install', function(e) {
     );
 });
 
-// when the browser fetches a url
-self.addEventListener('fetch', function(event) {
-    // either respond with the cached object or go ahead and fetch the actual url
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) {
-                // retrieve from cache
-                return response;
-            }
-            // fetch as normal
-            return fetch(event.request);
-        })
-    );
-});
+addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  return cache.match('/index.html');
+                });
+            });
+        }
+      })
+  );
+});  
