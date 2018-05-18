26f851547132d1caf33f5464fcd56dfe1ca518f9
philippglenn <peyj73@gmail.com>
2018-05-18 11:27:59 +0800

    added

---
+++ sw.js
@@ -0,0 +1,7 @@
+(function () {
+  'use strict';
+  importScripts('sw-toolbox.js');
+  toolbox.precache(["index.html","styles/style.css","styles/inline.css"]);
+  toolbox.router.get('/images/*', toolbox.cacheFirst);
+  toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+}());
